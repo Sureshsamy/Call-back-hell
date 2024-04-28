fe4f23550ecdcefeaa795799d814f05f170d5dd6
Sureshsamy <sureshsamy8973@gmail.com>
2024-04-28 21:01:15 +0530

    callback folder added

---
+++ index.js
@@ -0,0 +1,10 @@
+/*function display(){
+    console.log("the value printed in display function is" +value)
+}
+
+function add(a,b,display){
+    console.log("the input is "a","+b)
+    display(a+b)
+}
+add(10,2,display)*/
+
